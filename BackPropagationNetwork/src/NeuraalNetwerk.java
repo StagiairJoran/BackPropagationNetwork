82e1694b9cec507c967aff7b2e0761c0e42bf4a6
Joran De Boever <jorandeboever@MacBook-Pro-van-Joran.local>
2015-11-17 15:32:59 +0100

    lists van doubles

--- BackPropagationNetwork/src/NeuraalNetwerk.java
+++ BackPropagationNetwork/src/NeuraalNetwerk.java
@@ -3,4 +3,44 @@
  * on 17/11/15.
  */
 public class NeuraalNetwerk {
+    private double[] inputWaarden;
+
+    private double[][] eersteAxonen;
+
+    private double[] hiddenWaarden;
+    private double[] hiddenBiasWaarden;
+
+    private double[][] tweedeAxonen;
+
+    private double[] outputWaarden;
+    private double[] outputBiasWaarden;
+
+
+    public double[] getInputWaarden() {
+        return inputWaarden;
+    }
+
+    public double[][] getEersteAxonen() {
+        return eersteAxonen;
+    }
+
+    public double[] getHiddenWaarden() {
+        return hiddenWaarden;
+    }
+
+    public double[] getHiddenBiasWaarden() {
+        return hiddenBiasWaarden;
+    }
+
+    public double[][] getTweedeAxonen() {
+        return tweedeAxonen;
+    }
+
+    public double[] getOutputWaarden() {
+        return outputWaarden;
+    }
+
+    public double[] getOutputBiasWaarden() {
+        return outputBiasWaarden;
+    }
 }
