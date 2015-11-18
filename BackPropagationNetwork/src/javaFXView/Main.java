b9089243d2c5c464a8c7d6e1fd3cfe7fc8e78531
Joran De Boever <jorandeboever@MacBook-Pro-van-Joran.local>
2015-11-18 11:29:00 +0100

    JAVA FX begin

---
+++ BackPropagationNetwork/src/javaFXView/Main.java
@@ -0,0 +1,28 @@
+package javaFXView;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Joachim De Schryver & Joran De Boever
+ * on 18/11/15.
+ */
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample2.fxml"));
+        primaryStage.setTitle("Backpropagation");
+        primaryStage.setScene(new Scene(root, 600, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
